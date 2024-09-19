b708a21980e9c4600da425d7f5c5e946aeef046f
EduardoAndradeEJ6 <contato.eduardo.ti@gmail.com>
2024-09-19 19:10:37 -0300

    Diretório da aula 01

---
+++ Aula01/main.py
@@ -0,0 +1,69 @@
+#print('Ola mundo!!!')
+
+# string - cadeia de caracteres 
+# int - Números inteiros
+# float - Números reais, com virgula
+# bool - True or false
+# Tipagem dinamica, não precisa ficar especificando o tipo de cada variavel 
+# CTRL ALT N - Rodar código
+# apenas um = é apenas uma atribuição a uma variavel
+# + soma
+# - subtrair
+# * multiplicar
+# / divisão
+# % porcentagem resto da divisão
+# / / parte inteira da divisão
+
+
+#ano_nascim = 2000
+#ano_atual = 2024
+
+#idade = ano_atual - ano_nascim
+
+#print(idade)
+
+
+#n1 = 5
+#n2 = 2 
+
+#resultado = n1 // n2
+
+#print (resultado) 
+
+
+#------------- Atividade 01--------------- #
+
+
+print ('Hello World')
+
+mensagem = ('Algoritmos em Python')
+
+print (mensagem)
+
+nome = ('Eduardo')
+
+print ('Ola', nome)
+
+meunome = ('Eduardo Andrade')
+corfavorita = ('verde escuro')
+
+print(meunome,("minha cor favorita e"), corfavorita)
+
+
+profissão = ("Analista de suporte Pleno")
+local = ("Niteroi")
+
+print('Ola', nome , 'a sua profissao e',  profissão ,'e voce mora em', local)
+
+
+idade = 24
+altura = 1,74
+
+print( nome,  idade, altura)
+
+
+
+
+
+
+
